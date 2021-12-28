24e676bb691a25a5f51222b7c728857e588c5072
mabus <m.buscaroli@outlook.com>
2021-12-28 16:28:40 +0000

    Initial Commit

---
+++ SimpleMapKitApp/SimpleMapKitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleMapKitAppApp.swift
+//  SimpleMapKitApp
+//
+//  Created by Matteo on 28/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct SimpleMapKitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
