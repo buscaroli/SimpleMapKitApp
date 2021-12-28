85f16f92649840c65e18e363b0a6de8fe367560f
mabus <m.buscaroli@outlook.com>
2021-12-28 16:58:05 +0000

    Starting layout

--- SimpleMapKitApp/ContentView.swift
+++ SimpleMapKitApp/ContentView.swift
@@ -9,13 +9,39 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack (spacing: 0){
+            Color.blue
+                .edgesIgnoringSafeArea(.all)
+            
+            ZStack {
+                Color.black
+                    .edgesIgnoringSafeArea(.all)
+                    .frame(width: .infinity, height: 50)
+                
+                
+                HStack(spacing: 50) {
+                    Image(systemName: "star")
+                    
+                    Image(systemName: "heart")
+                    
+                    Image(systemName: "cross")
+                    
+                    Image(systemName: "person")
+                    
+                }
+                .font(.title)
+                .foregroundColor(.white)
+            }
+            
+            
+        }
     }
+        
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        
     }
 }
